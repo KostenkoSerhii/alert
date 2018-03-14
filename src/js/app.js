5cced45b4955a82ef137940507f6c0383e993efe
Serhii <kso_132@mail.ru>
2018-03-14 13:35:13 +0200

    home upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
+
+import sliders from './components/sliders.js';
 // import svgUseIt from 'svg-use-it';
-// import scrollMagic from './components/scrollmagic.js';
 // import techAnimation from './components/tech-animation.js';
 // import slick from 'slick-carousel';
 
 $(document).ready(function(){
-
+sliders();
 	// svgUseIt();
 	// scrollMagic();
 	// techAnimation();
---
+++ src/js/components/sliders.js
@@ -0,0 +1,25 @@
+import Slick from 'slick-carousel';
+function sliders(){
+	let prevBtn = `<div class="btn-prev"><i><?xml version="1.0" encoding="utf-8"?>
+	<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	viewBox="0 0 192 320" enable-background="new 0 0 192 320" xml:space="preserve">
+	<polygon points="192,300.6 171.3,320 0,160 171.3,0 192,19.3 41.5,160 "/>
+	</svg></i>
+	</div>`;
+	let nextBtn = `<div class="btn-next"><i><?xml version="1.0" encoding="utf-8"?>
+	<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	viewBox="0 0 192 320" enable-background="new 0 0 192 320" xml:space="preserve">
+	<polygon points="0,19.4 20.7,0 192,160 20.7,320 0,300.7 150.5,160 "/>
+	</svg></i>
+	</div>`;
+
+	$('.js-slider').slick({
+		slidesToShow: 1,
+		dots: false,
+		prevArrow: prevBtn,
+		nextArrow: nextBtn,
+		responsive: [
+		]
+	});
+}
+module.exports = sliders;
