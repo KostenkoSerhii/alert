31539599a7820fbecadbcc4b7919eeaefe53e417
Serhii <kso_132@mail.ru>
2018-03-15 15:40:47 +0200

    blog page

--- src/js/components/clickEvent.js
+++ src/js/components/clickEvent.js
@@ -13,3 +13,21 @@ $(".js-drop-block-toggler").on("click", function(e){
 		$content.slideDown(300);
 	}
 });
+
+$(".js-switch-btn").on("click", function(e){
+	e.preventDefault();
+	let $this = $(this);
+
+	if($this.hasClass('is-active')) return;
+
+	let target = $this.attr('data-switch'),
+	$parent = $this.parents(".js-switch-wrapper"),
+	$targetElems = $parent.find(`.js-switch-value[data-switch-value=${target}]`);
+
+
+	$('.js-switch-btn').removeClass('is-active');
+	$('.js-switch-value').removeClass('is-active');
+	$this.addClass('is-active');
+	$targetElems.addClass('is-active');
+
+});
